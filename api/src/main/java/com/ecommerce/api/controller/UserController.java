1f92268d2d48e3a24bbfb4f92a89f1552c199a81
João Miguel Galvao Ferraz <522021@pjro.local>
2026-07-21 12:03:11 -0400

    branch1: funcções do usur

--- api/src/main/java/com/ecommerce/api/controller/UserController.java
+++ api/src/main/java/com/ecommerce/api/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.ecommerce.api.controller;
 
+import com.ecommerce.api.service.UserService;
+import com.ecommerce.api.model.User;
+import com.ecommerce.api.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public ResponseEntity<List<User>> getALlUsers() {
+        List<User>
+    }
+
 }
